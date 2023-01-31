b167de014b8c9c64d42926222a7724491d81592a
Fergus Mitchell <2674244M@student.gla.ac.uk>
2023-01-31 22:28:58 +0000

    Finished 5.7

--- rango/models.py
+++ rango/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     def __str__(self):
-        return self.name
+        
+         return self.name
      
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -13,5 +17,6 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
     
-    def _str__(self):
-        return self.title
+    def __str__(self):
+        
+         return self.title
