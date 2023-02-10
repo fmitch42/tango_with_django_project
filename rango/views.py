693f58463f9edc8e964823655177d693d06ee9c9
Fergus Mitchell <2674244M@student.gla.ac.uk>
2023-02-10 10:43:14 +0000

    Finished Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from rango.forms import CategoryForm, UserForm, UserProfileForm, PageForm
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
 
 
 
@@ -17,6 +18,12 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html',context=context_dict)
+    return response
+
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -165,9 +172,35 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
 
+def get_server_side_cookie(request,cookie,default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
+
 
     
     
 def about(request):
-
-    return render(request, 'rango/about.html')
+    context_dict = {}
+    visitor_cookie_handler(request)#
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'rango/about.html', context = context_dict)
